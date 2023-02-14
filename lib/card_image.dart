117d142331f5f0b4adca4d67f1b838fab5e740ab
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2023-02-13 20:00:22 -0500

    App name is changed

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -6,12 +6,19 @@ class CardImage extends StatelessWidget{
 
   CardImage(this.pathImage);
 
+  //Best Relations
+  //width: width / 8.2,
+  //height: height / 4.3
+
   @override
   Widget build(BuildContext context) {
 
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     final card = Container(
-      height: 350.0,
-      width: 250.0,
+      width: width / 8.2,
+      height: height / 4.3,
       margin: const EdgeInsets.only(
         top: 80.0,
         left: 20.0
--- lib/header.dart
+++ lib/header.dart
@@ -1,16 +1,15 @@
+import 'package:colossal_hate/card_image_list.dart';
 import 'package:flutter/material.dart';
+
+import 'card_image.dart';
 
 class Header extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-
     return Container(
-      width: width / 8.2,
-      height: height / 4.3,
-      child: Image.network("https://i.scdn.co/image/ab6761610000e5ebff8e0c3d0ae582c24902509c"),
+      child: CardImage("assets/img/_DSC5109-2.jpg")
+      //child: Image.network("https://i.scdn.co/image/ab6761610000e5ebff8e0c3d0ae582c24902509c"),
     );
   }
 
