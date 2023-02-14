92d1b9a7ae0a39eb8f86b0e342703713dd4df452
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2023-02-13 20:05:23 -0500

    Screen relation in the header is added

--- lib/header.dart
+++ lib/header.dart
@@ -7,7 +7,12 @@ class Header extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Container(
+      width: width,
+      height: height / 3,
       child: CardImage("assets/img/_DSC5109-2.jpg")
       //child: Image.network("https://i.scdn.co/image/ab6761610000e5ebff8e0c3d0ae582c24902509c"),
     );
